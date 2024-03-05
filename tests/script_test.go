f86fd84e77b888a03c2faa9e494be17c0debbcff
pspiagicw <pspiagicw@gmail.com>
2024-03-05 08:48:23 +0000

    refactor(tests): Added more tests

--- tests/script_test.go
+++ tests/script_test.go
@@ -1,13 +1,50 @@
 package tests
 
-// import "testing"
+import (
+	"os/exec"
+	"testing"
 
-// func TestScripts(t *testing.T) {
-// 	tt := []string{
-// 		`(+ 1 2)`,
-// 	}
-//
-// 	for _, test := range tt {
-// 	}
-//
-// }
+	"github.com/gkampitakis/go-snaps/snaps"
+)
+
+type scriptTest struct {
+	name string
+	file string
+}
+
+func checkFile(t *testing.T, filename string) {
+	cmd := exec.Command("../hotshot", filename)
+	output, err := cmd.Output()
+	if err != nil {
+		t.Errorf("Error running interpreter! %v", err)
+	}
+	snaps.MatchSnapshot(t, string(output))
+}
+
+func TestScripts(t *testing.T) {
+	tt := []scriptTest{
+		{
+			name: "Hello World",
+			file: "files/hello-world.ht",
+		},
+		{
+			name: "Data Types",
+			file: "files/data-types.ht",
+		},
+		{
+			name: "Arithmetic Types",
+			file: "files/arithmetic.ht",
+		},
+		{
+			name: "Booleans",
+			file: "files/booleans.ht",
+		},
+	}
+
+	for _, test := range tt {
+		t.Run(test.name, func(t *testing.T) {
+			checkFile(t, test.file)
+		})
+	}
+
+}
